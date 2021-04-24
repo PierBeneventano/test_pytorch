0758b204d8b51caba28fae3986dff491a853964a
PierBeneventano <64017005+PierBeneventano@users.noreply.github.com>
2021-04-24 15:08:20 -0400

    added more datasets

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import torch.backends.cudnn as cudnn
 import torchvision
 import torchvision.transforms as transforms
 
+import matplotlib.pyplot as plt
+
 import os
 import argparse
 
@@ -16,8 +18,10 @@ from utils import progress_bar
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--dataset', choices=['MNIST', 'cifar10', 'cifar100'], default='cifar10', help='what dataset to use')
 parser.add_argument('--net', choices=['vgg', 'densenet', 'dla'], default='vgg', help='what model to train')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--batchsize', default=128, type=int, help='batchsize')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
 args = parser.parse_args()
@@ -26,32 +30,84 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
-# Data
+
+# Data I want all the data pipeline in another file
 print('==> Preparing data..')
+if args.dataset == 'cifar10':
+    stats = ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    crop=32
+    classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
+    input_dim = [1,1,32,32]
+elif args.dataset == 'cifar100':
+    stats = ((0.5074,0.4867,0.4411),(0.2011,0.1987,0.2025))
+    crop=32
+    input_dim = [1,1,32,32]
+elif args.dataset == 'MNIST':
+    stats = ((0.1307,), (0.3081,))
+    crop=28
+    input_dim = [1,1,28,28]
+
 transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
+    transforms.RandomCrop(crop, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transforms.Normalize(stats),
 ])
 
 transform_test = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transforms.Normalize(stats),
 ])
 
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+if args.dataset == 'cifar10':
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, args.batchsize, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
+    testset = torchvision.datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(
+        testset, args.batchsize, shuffle=False, num_workers=2)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
+elif args.dataset == 'cifar100':
+    trainset = torchvision.datasets.CIFAR100(
+        root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, args.batchsize, shuffle=True, num_workers=4)
+
+    testset = torchvision.datasets.CIFAR100(
+        root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(
+        testset, args.batchsize, shuffle=False, num_workers=4)
+
+elif args.dataset == 'MNIST':
+    trainset = torchvision.datasets.MNIST(
+        root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, args.batchsize, shuffle=True)
+
+    testset = torchvision.datasets.MNIST(
+        root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(
+        testset, args.batchsize, shuffle=False)
+
+# plot 6 examples of data point
+examples = enumerate(test_loader)
+batch_idx, (example_data, example_targets) = next(examples)
+example_data.shape
+
+fig = plt.figure()
+for i in range(6):
+  plt.subplot(2,3,i+1)
+  plt.tight_layout()
+  plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+  plt.title("Ground Truth: {}".format(example_targets[i]))
+  plt.xticks([])
+  plt.yticks([])
+fig
+
 
 # Model
 print('==> Building model..')
@@ -92,7 +148,6 @@ def train(epoch):
     total = 0
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         inputs, targets = inputs.to(device), targets.to(device)
-        print(inputs.shape)
         optimizer.zero_grad()
         outputs = net(inputs)
         loss = criterion(outputs, targets)
