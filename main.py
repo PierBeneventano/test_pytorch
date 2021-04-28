654aef976c837af44e218fb6b144c384220c7ce3
PierBeneventano <64017005+PierBeneventano@users.noreply.github.com>
2021-04-27 23:35:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -211,7 +211,7 @@ def test(epoch):
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
             inputs, targets = inputs.to(device), targets.to(device)
-            outputs = net(inputs)
+            outputs, aux = net(inputs)
             loss = criterion(outputs, targets)
 
             test_loss += loss.item()
