e94a42fa46be6a81d0a3bb6e9f8cfe192e6fc3a4
PierBeneventano <64017005+PierBeneventano@users.noreply.github.com>
2021-04-27 22:26:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ elif args.dataset == 'cifar100':
         testset, args.batchsize, shuffle=False, num_workers=4)
 
 elif args.dataset == 'MNIST':
-    datasets.MNIST.resources = [
+    torchvision.datasets.MNIST.resources = [
     ('https://ossci-datasets.s3.amazonaws.com/mnist/train-images-idx3-ubyte.gz', 'f68b3c2dcbeaaa9fbdd348bbdeb94873'),
     ('https://ossci-datasets.s3.amazonaws.com/mnist/train-labels-idx1-ubyte.gz', 'd53e105ee54ea40749a09fcbcd1e9432'),
     ('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images-idx3-ubyte.gz', '9fb629c4189551a2d022fa330f9573f3'),
