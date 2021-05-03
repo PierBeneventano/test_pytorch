26bbf04ea881c6ff127d3f7f973ea3dd472809df
PierBeneventano <64017005+PierBeneventano@users.noreply.github.com>
2021-05-03 02:58:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import os
 import argparse
 
 from models import *
-from utils import *
+import utils
 import optim_util
 
 
@@ -262,7 +262,7 @@ def train(epoch):
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+        utils.progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                      % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
 
@@ -287,7 +287,7 @@ def test(epoch):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            utils.progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                          % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
     # Save checkpoint.
