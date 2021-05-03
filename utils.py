6ad8434377a934a93dde1848857f02e3ebca60fd
PierBeneventano <64017005+PierBeneventano@users.noreply.github.com>
2021-05-03 03:00:22 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -31,15 +31,15 @@ def init_params(net):
     for m in net.modules():
         if isinstance(m, nn.Conv2d):
             init.kaiming_normal(m.weight, mode='fan_out')
-            if m.bias:
-                init.constant(m.bias, 0)
+            # if m.bias:
+            init.constant(m.bias, 0)
         elif isinstance(m, nn.BatchNorm2d):
             init.constant(m.weight, 1)
             init.constant(m.bias, 0)
         elif isinstance(m, nn.Linear):
             init.normal(m.weight, std=1e-3)
-            if m.bias:
-                init.constant(m.bias, 0)
+            # if m.bias:
+            init.constant(m.bias, 0)
 
 
 _, term_width = os.popen('stty size', 'r').read().split()
