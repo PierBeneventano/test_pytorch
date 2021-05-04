12b728b0c8d3ada5dbeaf1896bb04ce5bab428b3
PierBeneventano <64017005+PierBeneventano@users.noreply.github.com>
2021-05-04 01:24:56 -0400

    Update make_makefile.py

--- make_makefile.py
+++ make_makefile.py
@@ -2,16 +2,24 @@ import os
 import numpy as np
 cwd = os.getcwd()   # get the current working directory
 
-def create_makefile(choices_array):
+def create_makefile(choice_dict):
     f = open(f"{cwd}/makefile","w")
     f.write(".PHONY = help setup test run clean environment\n\n")
     f.write(".DEFAULT_GOAL = setup\n")
     f.write("setup:\n")
-    for choice in choices_array:
-        f.write(f"\t@python main.py --net {choice}\n")
+    for net_choice in choice_dict['net']:
+        for dataset_choice in choice_dict['dataset']:
+            f.write(f"\t@python main.py --net {choice} --dataset {dataset_choice}\n")
     
 
 if __name__ == "__main__":
-    choices_array = np.array(['vgg', 'densenet', 'dla'])
-    create_makefile(choices_array)
+    # creating choice dictionary
+    choice_dict = []
+
+    net_choices = np.array(['vgg', 'densenet', 'dla'])
+    dataset_choices = np.array(['cifar10', 'MNIST'])
+
+    choice_dict['net'] = net_choices
+    choice_dict['dataset'] = dataset_choices
+    create_makefile(choices_dict)
     
