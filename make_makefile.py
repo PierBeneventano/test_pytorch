e998553ca5185283330e704bfba07e186f63ce17
PierBeneventano <64017005+PierBeneventano@users.noreply.github.com>
2021-05-04 01:47:05 -0400

    make_makefile

--- make_makefile.py
+++ make_makefile.py
@@ -7,9 +7,12 @@ def create_makefile(choice_dict):
     f.write(".PHONY = help setup test run clean environment\n\n")
     f.write(".DEFAULT_GOAL = setup\n")
     f.write("setup:\n")
-    for net_choice in choice_dict['net']:
-        for dataset_choice in choice_dict['dataset']:
-            f.write(f"\t@python main.py --net {net_choice} --dataset {dataset_choice}\n")
+    for ln_choice in choice_dict['label_noise_prob']:
+        if ln_choice != 0:
+            for ln_sched_choice in choice_dict['ln_sched']:
+                f.write(f"\t@python main.py --dataset 'MNIST' --label_noise {ln_choice} --ln_sched {ln_sched_choice}\n")
+                for net_choice in choice_dict['net']:
+                    f.write(f"\t@python main.py --net {net_choice} --label_noise {ln_choice} --ln_sched {ln_sched_choice}\n")
     
 
 if __name__ == "__main__":
@@ -17,9 +20,12 @@ if __name__ == "__main__":
     choice_dict = {}
 
     net_choices = np.array(['vgg', 'densenet', 'dla'])
-    dataset_choices = np.array(['cifar10', 'MNIST'])
+    label_noise_choices = np.array([0.5, 0.2, 0.1, 0.0])
+    ln_sched_choices = np.array(['decay', 'fixed'])
 
     choice_dict['net'] = net_choices
-    choice_dict['dataset'] = dataset_choices
+    choice_dict['label_noise_prob'] = label_noise_choices
+    choice_dict['ln_sched'] = ln_sched_choices
+
     create_makefile(choice_dict)
     
