7c796269f77f8f9e2495df077598c15376f3b274
PierBeneventano <64017005+PierBeneventano@users.noreply.github.com>
2021-05-09 20:03:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -141,12 +141,12 @@ elif args.dataset == 'cifar100':
         testset, args.batchsize, shuffle=False, num_workers=4)
 
 elif args.dataset == 'MNIST':
-    torchvision.datasets.MNIST.resources = [
-    ('https://ossci-datasets.s3.amazonaws.com/mnist/train-images-idx3-ubyte.gz', 'f68b3c2dcbeaaa9fbdd348bbdeb94873'),
-    ('https://ossci-datasets.s3.amazonaws.com/mnist/train-labels-idx1-ubyte.gz', 'd53e105ee54ea40749a09fcbcd1e9432'),
-    ('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images-idx3-ubyte.gz', '9fb629c4189551a2d022fa330f9573f3'),
-    ('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-labels-idx1-ubyte.gz', 'ec29112dd5afa0611ce80d1b7f02629c')
-    ]
+    # torchvision.datasets.MNIST.resources = [
+    # ('https://ossci-datasets.s3.amazonaws.com/mnist/train-images-idx3-ubyte.gz', 'f68b3c2dcbeaaa9fbdd348bbdeb94873'),
+    # ('https://ossci-datasets.s3.amazonaws.com/mnist/train-labels-idx1-ubyte.gz', 'd53e105ee54ea40749a09fcbcd1e9432'),
+    # ('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images-idx3-ubyte.gz', '9fb629c4189551a2d022fa330f9573f3'),
+    # ('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-labels-idx1-ubyte.gz', 'ec29112dd5afa0611ce80d1b7f02629c')
+    # ]
     trainset = torchvision.datasets.MNIST(
         root='./data', train=True, download=True, 
         transform=torchvision.transforms.Compose([
