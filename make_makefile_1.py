1b1f413e12a6b440b9b15b7fd54ddad456d40c47
PierBeneventano <64017005+PierBeneventano@users.noreply.github.com>
2021-05-09 23:18:19 -0400

    make_files

---
+++ make_makefile_1.py
@@ -0,0 +1,42 @@
+import os
+import numpy as np
+cwd = os.getcwd()   # get the current working directory
+
+def create_makefile(choice_dict):
+    f = open(f"{cwd}/makefile",'w')
+    f.write(".PHONY = help setup test run clean environment\n\n")
+    f.write(".DEFAULT_GOAL = setup\n")
+    f.write("setup:\n")
+
+    
+    # full_batch
+    f.write(f"\t@python main.py --dataset 'MNIST' --batchsize 1024\n")
+    #for net_choice in choice_dict['net']:
+    f.write(f"\t@python main.py  --batchsize 1024\n")
+
+    # Gradient noise
+    for i in [128, 1024]:
+        for lg_choice in choice_dict['gaussian_noise_sigma']:
+            for lg_sched_choice in choice_dict['noise_sched']:
+                f.write(f"\t@python main.py --dataset 'MNIST' --gaussian_noise {lg_choice} --noise_sched {lg_sched_choice} --batchsize {i} \n")
+                f.write(f"\t@python main.py  --gaussian_noise {lg_choice} --noise_sched {lg_sched_choice} --batchsize {i} \n")
+
+
+if __name__ == "__main__":
+    # creating choice dictionary
+    choice_dict = {}
+
+    net_choices = np.array(['vgg', 'densenet', 'dla'])
+    label_noise_choices = np.array([0.5, 0.2, 0.1])
+    g_noise_choices = np.array([0.2, 0.1, 0.02])
+    noise_sched_choices = np.array(['decay', 'fixed'])
+    lr_choices = np.array([0.1, 0.01, 0.001])
+
+    choice_dict['net'] = net_choices
+    choice_dict['label_noise_prob'] = label_noise_choices
+    choice_dict['noise_sched'] = noise_sched_choices
+    choice_dict['gaussian_noise_sigma'] = g_noise_choices
+    choice_dict['learning_rate'] = lr_choices
+
+    create_makefile(choice_dict)
+    
