3377964f99cae46dd7a79815ac713f20fd90ca0d
PierBeneventano <64017005+PierBeneventano@users.noreply.github.com>
2021-05-09 20:20:10 -0400

    Update make_makefile.py

--- make_makefile.py
+++ make_makefile.py
@@ -13,6 +13,12 @@ def create_makefile(choice_dict):
     for net_choice in choice_dict['net']:
         f.write(f"\t@python main.py --net {net_choice}\n")
 
+    # # plain sgd
+    # for lr_choice in choice_dict['learning_rate']:
+    #     f.write(f"\t@python main.py --dataset 'MNIST'\n")
+    #     for net_choice in choice_dict['net']:
+    #         f.write(f"\t@python main.py --net {net_choice}\n")
+
     # full_batch
     f.write(f"\t@python main.py --dataset 'MNIST' --batchsize 1024\n")
     for net_choice in choice_dict['net']:
@@ -51,11 +57,13 @@ if __name__ == "__main__":
     label_noise_choices = np.array([0.5, 0.2, 0.1])
     g_noise_choices = np.array([0.2, 0.1, 0.02])
     noise_sched_choices = np.array(['decay', 'fixed'])
+    lr_choices = np.array([0.1, 0.01, 0.001])
 
     choice_dict['net'] = net_choices
     choice_dict['label_noise_prob'] = label_noise_choices
     choice_dict['noise_sched'] = noise_sched_choices
     choice_dict['gaussian_noise_sigma'] = g_noise_choices
+    choice_dict['learning_rate'] = lr_choices
 
     create_makefile(choice_dict)
     
