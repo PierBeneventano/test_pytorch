b87f2ed83f68425f001ed52d0f97b3ffbbcc4c08
PierBeneventano <64017005+PierBeneventano@users.noreply.github.com>
2021-05-14 13:50:32 -0400

    Update main_MNIST.py

--- main_MNIST.py
+++ main_MNIST.py
@@ -29,16 +29,17 @@ parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
                         help='learning rate (default: 1.0)')
 parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
                         help='Learning rate step gamma (default: 0.7)')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-parser.add_argument('--dry-run', action='store_true', default=False,
-                        help='quickly check a single pass')
+# parser.add_argument('--no-cuda', action='store_true', default=False,
+#                         help='disables CUDA training')
+# parser.add_argument('--dry-run', action='store_true', default=False,
+#                         help='quickly check a single pass')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-parser.add_argument('--save-model', action='store_true', default=False,
-                        help='For Saving the current Model')
+# parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+#                         help='how many batches to wait before logging training status')
+# parser.add_argument('--save-model', action='store_true', default=False,
+#                         help='For Saving the current Model')
+
 # Noise injection related
 parser.add_argument('--gaussian_noise', default=0, type=float, help='the entity (the standard deviation sigma) of the gaussian noise.')
 parser.add_argument('--input_gaussian_noise', default=0, type=float, help='the entity (the standard deviation sigma) of the input gaussian noise.')
