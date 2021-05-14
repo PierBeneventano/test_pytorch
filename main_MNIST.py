f756302068f64b9c59eb6ac7360934f30548a193
PierBeneventano <64017005+PierBeneventano@users.noreply.github.com>
2021-05-14 13:54:15 -0400

    Update main_MNIST.py

--- main_MNIST.py
+++ main_MNIST.py
@@ -31,14 +31,9 @@ parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
                         help='Learning rate step gamma (default: 0.7)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                         help='disables CUDA training')
-# parser.add_argument('--dry-run', action='store_true', default=False,
-#                         help='quickly check a single pass')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
-# parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-#                         help='how many batches to wait before logging training status')
-# parser.add_argument('--save-model', action='store_true', default=False,
-#                         help='For Saving the current Model')
+
 
 # Noise injection related
 parser.add_argument('--gaussian_noise', default=0, type=float, help='the entity (the standard deviation sigma) of the gaussian noise.')
