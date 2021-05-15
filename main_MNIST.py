9f68588dd710aeef8bffdaaf97cb1d8110fbde49
PierBeneventano <64017005+PierBeneventano@users.noreply.github.com>
2021-05-14 23:17:58 -0400

    Update main_MNIST.py

--- main_MNIST.py
+++ main_MNIST.py
@@ -20,7 +20,7 @@ parser.add_argument('--batchsize', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
 parser.add_argument('--test_batchsize', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=14, metavar='N',
+parser.add_argument('--epochs', type=int, default=30, metavar='N',
                         help='number of epochs to train (default: 30)')
 parser.add_argument('--net', choices=['CNN', 'linear'], default='CNN', help='what model to train')
 parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
