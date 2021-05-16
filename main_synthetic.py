c43413238d7e74f887f5325c9ff122932c089cfb
PierBeneventano <64017005+PierBeneventano@users.noreply.github.com>
2021-05-16 13:31:11 -0400

    Update main_synthetic.py

--- main_synthetic.py
+++ main_synthetic.py
@@ -107,7 +107,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(output, target)
+        loss = F.cross_entropy(output, target)
         loss.backward()
         optimizer.step()
         
